a927bdfb6002d985d1bfb9243e8d369345007a93
kasha <kasha1147@gmail.com>
2021-06-09 02:17:28 +0300

    Wrote More hashfunction but doesn't test it

--- second_try/second_try.cpp
+++ second_try/second_try.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -10,6 +11,10 @@ __int64 S(__int64 a);
 __int64 P(__int64 a);
 __int64 L(__int64 a);
 __int16 mulOnMatrix(__int16 vector);
+__int64 gN(__int64 h, __int64 m, __int64 N);
+__int64 E(__int64 K, __int64 m);
+__int64 H(vector<char> M);
+__int64 cutM(vector<char> M);
 
 __int64 pi[] = { 15, 9, 1, 7, 13, 12, 2, 8, 6, 5, 14, 3, 0, 11, 4, 10 };
 __int8 tau[] = { 0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15 };
@@ -30,11 +35,9 @@ __int64 X(__int64 k, __int64 a)
 __int64 S(__int64 a)
 {
 	unsigned __int64 res = 0;
-	unsigned __int64 virezatel = 0xf;
 	for (int i = 0; i < 16; i++) {
-		__int8 part = (__int8)((a & virezatel) >> (i * 4));
+		__int8 part = (__int8)((a >> (i * 4)) & 0xf);
 		res ^= (pi[part] << (i * 4));
-		virezatel <<= 4;
 	}
 	return res;
 }
@@ -67,11 +70,59 @@ __int16 mulOnMatrix(__int16 vector)
 	__int16 res = 0;
 	__int16 check = 0x8000;
 	for (int i = 0; i < 16; i++) {
-		if ((vector & check) != 0)
+		if ((vector & check) != 0) {
 			res ^= matrixA[i];
+		}
 		check >> 1;
 	}
 	return res;
+}
+
+__int64 gN(__int64 h, __int64 m, __int64 N)
+{
+	return E(L(P(S(h ^ N))), m) ^ h ^ m;
+}
+
+__int64 E(__int64 K, __int64 m)
+{
+	for (int i = 0; i < 8; i++) {
+		m = L(P(S(X(K, m))));
+		K = L(P(S(K ^ C[i])));
+	}
+
+	return X(K, m);
+}
+
+__int64 H(vector<char> M)
+{
+	__int64 h = rand();
+	__int64 N = 0, sigma = 0;
+	__int64 m = 0;
+
+	while (M.size() >= 8) {
+		m = cutM(M);
+		h = gN(h, m, N);
+		N ^= 64;
+		sigma ^= m;
+		M = { M.begin(), M.end() - 8 };
+	}
+	//add 1 to m
+	h = gN(h, m, N);
+	N ^= M.size();
+	sigma ^= m;
+	return gN(h, N, 0);
+}
+
+__int64 cutM(vector<char> M)
+{
+	vector<char> toLongLong = { M.end() - 8, M.end() };
+	__int64 result = 0;
+
+	for (int i = 0; i < toLongLong.size(); i++) {
+		result ^= ((__int64)toLongLong[i]) << ((7 - i) * 8);
+	}
+
+	return result;
 }
 
 int main()
@@ -84,4 +135,3 @@ int main()
 	__int64 lpsa = L(psa);
 	cout << hex << "LPS(a): " << lpsa << endl;
 }
-
