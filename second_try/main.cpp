eff84d19f1a59c5598a00dd9f05163f12b4568ff
Вячеслав Савушкин <kasha1147@gmail.com>
2021-12-13 23:09:27 +0300

    not working diamond structure

--- second_try/main.cpp
+++ second_try/main.cpp
@@ -9,128 +9,145 @@ constexpr auto LEN = 8;
 using namespace std::chrono;
 
 
-//static const uint8_t PI[16] = { 15, 9, 1, 7, 13, 12, 2, 8, 6, 5, 14, 3, 0, 11, 4, 10 };
-//static const uint8_t TAU[16] = { 0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15 };
-//static const uint16_t A[16] = { 0x3a22, 0x8511, 0x4b99, 0x2cdd,
-//						0x483b, 0x248c, 0x1246, 0x9123,
-//						0x59e5, 0xbd7b, 0xcfac, 0x6e56,
-//						0xac52, 0x56b1, 0xb3c9, 0xc86d };
-//
-//static const uint8_t C[8][8] = {
-//	{ 0xc0, 0x16, 0x46, 0x33, 0x57, 0x5a, 0x96, 0x99 },
-//	{ 0x92, 0x5b, 0x4e, 0xf4, 0x9a, 0x5e, 0x71, 0x74 },
-//	{ 0x86, 0xa8, 0x9c, 0xdc, 0xf6, 0x73, 0xbe, 0x26 },
-//	{ 0x18, 0x85, 0x55, 0x8f, 0x0e, 0xac, 0xa3, 0xf1 },
-//	{ 0xdc, 0xfc, 0x5b, 0x89, 0xe3, 0x5e, 0x84, 0x39 },
-//	{ 0x54, 0xb9, 0xed, 0xc7, 0x89, 0x46, 0x4d, 0x23 },
-//	{ 0xf8, 0x0d, 0x49, 0xaf, 0xde, 0x04, 0x4b, 0xf9 },
-//	{ 0x8c, 0xbb, 0xdf, 0x71, 0xcc, 0xaa, 0x43, 0xf1 }
-//};
-//
-//uint8_t* L(uint8_t* block)
-//{
-//	uint16_t* tmp_res = new uint16_t[4];
-//	std::fill_n(tmp_res, 4, 0);
-//
-//	for (int i = 0; i < 4; i++)
-//	{
-//		for (int j = 0; j < 16; j++)
-//		{
-//			if (j < 8)
-//			{
-//				if (block[2 * i] & (1 << j))
-//				{
-//					tmp_res[i] ^= A[7 - j];
-//				}
-//			}
-//			else
-//			{
-//				if (block[2 * i + 1] & (1 << (j - 8)))
-//				{
-//					tmp_res[i] ^= A[15 - (j - 8)];
-//				}
-//			}
-//		}
-//	}
-//
-//	uint8_t* res = new uint8_t[8];
-//	for (int i = 0; i < 8; i++)
-//	{
-//		if (i % 2)
-//		{
-//			res[i] = tmp_res[i / 2] & 0xff;
-//		}
-//		else
-//		{
-//			res[i] = tmp_res[i / 2] >> 8;
-//		}
-//	}
-//
-//	return res;
-//}
-//
-//uint8_t* P(uint8_t* block)
-//{
-//	uint8_t* res = new uint8_t[8];
-//	std::fill_n(res, 8, 0);
-//
-//	for (int i = 0; i < 16; i++) {
-//		const uint8_t index = TAU[i];
-//		const uint8_t block_index = index / 2;
-//		const uint8_t block_value = (index % 2 == 0) ? block[block_index] >> 4 : block[block_index] & 0xf;
-//		res[i / 2] ^= (i % 2 == 0) ? (block_value << 4) : block_value;
-//	}
-//
-//	return res;
-//}
-//
-//uint8_t* S(uint8_t* block)
-//{
-//	uint8_t* res = new uint8_t[8];
-//
-//	for (int i = 0; i < 8; i++)
-//	{
-//		const uint8_t left_state = block[i] >> 4;
-//		const uint8_t right_state = block[i] & 0xf;
-//		res[i] = (PI[left_state] << 4) ^ PI[right_state];
-//	}
-//
-//	return res;
-//}
-//
-//void X(const uint8_t* a, const uint8_t* b, uint8_t* res)
-//{
-//	for (int i = 0; i < 8; i++)
-//	{
-//		res[i] = a[i] ^ b[i];
-//	}
-//}
-//
-//uint8_t* get_key(uint8_t* K, int i)
-//{
-//	return L(P(S(X(K, C[i]))));
-//}
-//
-//uint8_t* E(uint8_t* K, uint8_t* m)
-//{
-//	uint8_t* res = X(K, m);
-//	uint8_t* Ki = new uint8_t[8];
-//	memcpy(Ki, K, 8);
-//
-//	for (int i = 0; i < 8; i++)
-//	{
-//		res = L(P(S(res)));
-//		Ki = get_key(Ki, i);
-//		res = X(Ki, res);
-//	}
-//
-//	return res;
-//}
-//
-//uint8_t* gN(uint8_t* h, uint8_t* m, uint8_t* N)
-//{
-//	return X(X(E(L(P(S(X(h, N)))), m), h), m);
-//}
+static const uint8_t PI[16] = { 15, 9, 1, 7, 13, 12, 2, 8, 6, 5, 14, 3, 0, 11, 4, 10 };
+static const uint8_t TAU[16] = { 0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15 };
+static const uint16_t A[16] = { 0x3a22, 0x8511, 0x4b99, 0x2cdd,
+						0x483b, 0x248c, 0x1246, 0x9123,
+						0x59e5, 0xbd7b, 0xcfac, 0x6e56,
+						0xac52, 0x56b1, 0xb3c9, 0xc86d };
+
+static const uint8_t C[8][8] = {
+	{ 0xc0, 0x16, 0x46, 0x33, 0x57, 0x5a, 0x96, 0x99 },
+	{ 0x92, 0x5b, 0x4e, 0xf4, 0x9a, 0x5e, 0x71, 0x74 },
+	{ 0x86, 0xa8, 0x9c, 0xdc, 0xf6, 0x73, 0xbe, 0x26 },
+	{ 0x18, 0x85, 0x55, 0x8f, 0x0e, 0xac, 0xa3, 0xf1 },
+	{ 0xdc, 0xfc, 0x5b, 0x89, 0xe3, 0x5e, 0x84, 0x39 },
+	{ 0x54, 0xb9, 0xed, 0xc7, 0x89, 0x46, 0x4d, 0x23 },
+	{ 0xf8, 0x0d, 0x49, 0xaf, 0xde, 0x04, 0x4b, 0xf9 },
+	{ 0x8c, 0xbb, 0xdf, 0x71, 0xcc, 0xaa, 0x43, 0xf1 }
+};
+
+void L(uint8_t* block)
+{
+	uint16_t* tmp_res = new uint16_t[4];
+	std::fill_n(tmp_res, 4, 0);
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 16; j++)
+		{
+			if (j < 8)
+			{
+				if (block[2 * i] & (1 << j))
+				{
+					tmp_res[i] ^= A[7 - j];
+				}
+			}
+			else
+			{
+				if (block[2 * i + 1] & (1 << (j - 8)))
+				{
+					tmp_res[i] ^= A[15 - (j - 8)];
+				}
+			}
+		}
+	}
+
+	uint8_t* res = new uint8_t[8];
+	for (int i = 0; i < 8; i++)
+	{
+		if (i % 2)
+		{
+			res[i] = tmp_res[i / 2] & 0xff;
+		}
+		else
+		{
+			res[i] = tmp_res[i / 2] >> 8;
+		}
+	}
+
+	memcpy(block, res, 8);
+	delete[] tmp_res;
+	delete[] res;
+}
+
+void P(uint8_t* block)
+{
+	uint8_t* res = new uint8_t[8];
+	std::fill_n(res, 8, 0);
+
+	for (int i = 0; i < 16; i++) {
+		const uint8_t index = TAU[i];
+		const uint8_t block_index = index / 2;
+		const uint8_t block_value = (index % 2 == 0) ? block[block_index] >> 4 : block[block_index] & 0xf;
+		res[i / 2] ^= (i % 2 == 0) ? (block_value << 4) : block_value;
+	}
+	memcpy(block, res, 8);
+	delete[] res;
+}
+
+void S(uint8_t* block)
+{
+	uint8_t* res = new uint8_t[8];
+
+	for (int i = 0; i < 8; i++)
+	{
+		const uint8_t left_state = block[i] >> 4;
+		const uint8_t right_state = block[i] & 0xf;
+		res[i] = (PI[left_state] << 4) ^ PI[right_state];
+	}
+	memcpy(block, res, 8);
+	delete[] res;
+}
+
+void X(const uint8_t* a, const uint8_t* b, uint8_t* res)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		res[i] = a[i] ^ b[i];
+	}
+}
+
+void get_key(uint8_t* K, int i)
+{
+	X(K, C[i], K);
+	S(K);
+	P(K);
+	L(K);
+}
+
+void E(uint8_t* K, uint8_t* m, uint8_t* res)
+{
+	X(K, m, res);
+
+	for (int i = 0; i < 8; i++)
+	{
+		S(res);
+		P(res);
+		L(res);
+		get_key(K, i);
+		X(K, res, res);
+	}
+}
+
+void gN(uint8_t* h, uint8_t* m, uint8_t* N)
+{
+	uint8_t* K = new uint8_t[8];
+	uint8_t* tmp_res = new uint8_t[8];
+
+	X(h, N, K);
+	S(K);
+	P(K);
+	L(K);
+
+	E(K, m, tmp_res);
+
+	X(tmp_res, h, tmp_res);
+	X(tmp_res, m, h);
+
+	delete[] K;
+	delete[] tmp_res;
+}
 
 
 void print_array(uint8_t* arr)
@@ -542,7 +559,64 @@ int main()
 	
 	//diamond_structure(2);
 
+
 	uint8_t* a = new uint8_t[8];
+	uint8_t* b = new uint8_t[8];
+	uint8_t* res = new uint8_t[8];
+	for (int i = 0; i < 8; i++)
+	{
+		a[i] = i + 1;
+		b[i] = 8 - i;
+	}
+	X(a, b, res);
+	print_array(a);
+	print_array(b);
+	std::cout << "X operation" << std::endl;
+	print_array(res);
+	std::cout << std::endl;
+
+	uint8_t s[8] = { 0xf2, 0x52, 0xb7, 0x16, 0x8d, 0x7d, 0x7f, 0x71 };
+	S(s);
+	std::cout << "S operation" << std::endl;
+	print_array(s);
+	uint8_t* s1 = new uint8_t[8];
+	s1[0] = 0xf2; s1[1] = 0x52; s1[2] = 0xb7; s1[3] = 0x16; s1[4] = 0x8d; s1[5] = 0x7d; s1[6] = 0x7f; s1[7] = 0x71;
+	S(s1);
+	std::cout << "S operation" << std::endl;
+	print_array(s1);
+	std::cout << std::endl;
+
+	uint8_t p[8] = { 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef };
+	P(p);
+	std::cout << "P operation" << std::endl;
+	print_array(p);
+	uint8_t p1[8] = { 0xa1, 0xc1, 0x38, 0x92, 0x6b, 0x8b, 0x8a, 0x89 };
+	P(p1);
+	std::cout << "P operation" << std::endl;
+	print_array(p1);
+	std::cout << std::endl;
+
+	uint8_t l[8] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+	L(l);
+	std::cout << "L operation" << std::endl;
+	print_array(l);
+	uint8_t l1[8] = { 0xb8, 0x1f, 0xcb, 0xc5, 0x5e, 0x94, 0xf3, 0xcf };
+	L(l1);
+	std::cout << "L operation" << std::endl;
+	print_array(l1);
+	std::cout << std::endl;
+
+	uint8_t h[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	uint8_t N[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	uint8_t gn[8] = {0x01, 0xd4, 0x44, 0x90, 0x7e, 0xfb, 0x8c, 0xf7 };
+	gN(h, gn, N);
+	std::cout << "gN operation" << std::endl;
+	print_array(h);
+	print_array(gn);
+	print_array(N);
+	std::cout << std::endl;
+	
+	/*uint8_t* a = new uint8_t[8];
 	for (int i = 0; i < 8; i++)
 	{
 		a[i] = i;
@@ -554,7 +628,7 @@ int main()
 
 	unsigned long long int b = 64;
 	number2array(b, a);
-	print_array(a);
+	print_array(a);*/
 	
 	/*vector<uint8_t*> a;
 	uint8_t b[4] = {1, 2, 3, 4};
